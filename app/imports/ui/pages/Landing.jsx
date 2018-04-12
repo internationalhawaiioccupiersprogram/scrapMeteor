b54b12f589494464fe3569ddb5f3aa3882b69c1d
victoria-soto <vjaramil@hawaii.edu>
2018-04-12 09:16:56 -1000

    changed formatting--gonna put a better pic

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -11,24 +11,22 @@ class Landing extends React.Component {
           <Grid container stackable centered columns={3}>
 
             <Grid.Column textAlign='center'>
-              <Icon size='huge' name='address card'/>
+              <Icon size='huge' inverted name='address card'/>
               <Header as='h1'>List of Vendors</Header>
               <Header as='h3'>Browse the list of Food Vendors and their locations available at UH Manoa</Header>
             </Grid.Column>
 
             <Grid.Column textAlign='center'>
-              <Icon size='huge' name='coffee'/>
+              <Icon size='huge' inverted name='coffee'/>
               <Header as='h1'>Vendor Page</Header>
               <Header as='h3'>For each vendor, you can view their menu, location, and bussiness hours.</Header>
             </Grid.Column>
 
             <Grid.Column textAlign='center'>
-              <Icon size='huge' name='checked calendar'/>
-              <Header as='h1'></Header>
-              <Header as='h3'>Each time you make a contact with a contact, you can write a note that summarizes
-                the
-                conversation.
-                This note is saved along with a time stamp with the contact.</Header>
+              <Icon size='huge' inverted name='checked calendar'/>
+              <Header as='h1'>Browse Food Categories</Header>
+              <Header as='h3'>Craving Sushi, Pho, Pizza, or want to try something new? Search by food
+                categories.</Header>
             </Grid.Column>
           </Grid>
         </div>
